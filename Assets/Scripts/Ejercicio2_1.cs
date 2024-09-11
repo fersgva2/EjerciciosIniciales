241ba957124580f7e4280d2e8d7d96ee73d254fb
fersgva2 <fersgva2@gmail.com>
2024-09-11 21:22:00 +0200

    11/09

--- Assets/Scripts/Ejercicio2_1.cs
+++ Assets/Scripts/Ejercicio2_1.cs
@@ -2,23 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicio1_1 : MonoBehaviour
+public class Ejercicio2_1 : MonoBehaviour
 {
-    //Declarar variables
-    int vida = 10;
-
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Empiezo con" + vida);
-
-        vida -= 2;
-
-
-
-
-
-
+        
     }
 
     // Update is called once per frame
